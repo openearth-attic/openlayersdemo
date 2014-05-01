b56a583229f67f55efc2bb99831c5ad7743bbc9e
Frank Keppel <frank.keppel@deltares.nl>
2014-05-01 16:52:46 +0200

    Added timestep and colorrange to layer

--- ecostress/config/Options.js
+++ ecostress/config/Options.js
@@ -766,7 +766,7 @@ Heron.options.map.layers = [
      * ------------------------------ */
     new OpenLayers.Layer.WMS(
             "PACE",
-            Heron.OpenEarth.urls.PACE,
+            Heron.OpenEarth.urls.PACE + 'TIME=2009-01-31T06:59:59.999Z&COLORSCALERANGE=-1,1&',
             {layers: "waterlevel", format: "image/png", transparent: true},
             {isBaseLayer: false, singleTile: true, visibility: false, featureInfoFormat: "application/vnd.ogc.gml", alpha: true, opacity: 0.7}
 
